1f928e84990f5558d6e3f8a9da384f0fd293130d
Infendum <69817375+Infendum@users.noreply.github.com>
2022-01-24 20:20:37 +0600

    Update 24.01.2022.cpp

--- 24.01.2022.cpp
+++ 24.01.2022.cpp
@@ -28,3 +28,38 @@ int main() {
 	}
 
 }
+
+задание номер 2
+
+#include <iostream>
+#include <Windows.h>
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int guessNumber;
+	int low = 1;
+	int high = 100;
+	int mid, compare;
+	int randomNumber;
+	std::cout << "ты загадал число, я отгадаю его" << std::endl;
+
+	do {
+		mid = (low + high) / 2;
+		printf("Ваше число %d ?", mid);
+		std::cout << "1 - Число больше\n2 - Число меньше\n0 - Да!" <<std::endl;
+		std::cin >> compare;
+
+		if (compare == 1) {
+			low = mid + 1;
+		}
+		else if (compare == 2) {
+			high = mid + 1;
+		}
+		else if (compare == 0) {
+			printf("Вашу число %d", mid);
+		}
+
+	} while (compare != 0);
+}
