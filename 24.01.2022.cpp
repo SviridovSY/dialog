b80ad25dd6a2f0d27c7adfac65fc2a725dce0742
Infendum <69817375+Infendum@users.noreply.github.com>
2022-01-24 21:36:21 +0600

    Update 24.01.2022.cpp

--- 24.01.2022.cpp
+++ 24.01.2022.cpp
@@ -73,7 +73,7 @@ int main() {
 
 
 
-
+способ один
 
 #include <iostream>
 #include <Windows.h>
@@ -110,3 +110,70 @@ int main() {
 	output(arr, size);
 
 }
+
+
+способ два
+
+
+#include <iostream>
+#include <Windows.h>
+
+
+void output(int* arr, int size) {
+	for (int i = 0; i < size; i++) {
+		std::cout << arr[i] << ' ';
+	}
+}
+
+
+int findMin(int* arr, int size) {
+	int min = arr[0];
+	int indexMin = 0;
+	for (int i = 0; i < size; i++) {
+		if (min > arr[i]) {
+			min = arr[i];
+			indexMin = 1;
+		}
+	}
+
+	return indexMin;
+}
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	const int size = 10;
+	int arr[size];
+
+	int newarray[size];
+	int minEL;
+
+	for (int i = 0; i < size; i++) {
+		arr[i] = rand() % 1000;
+	}
+	output(arr, size);
+	int temp;
+	for (int i = 0; i < size - 1; i++) {
+		for (int j = 0; j < size - i - 1; j++) {
+			if (arr[j] > arr[j + 1]) {
+				temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+
+	output(arr, size);
+
+	for (int i = 0; i < size; i++) {
+		minEL = findMin(arr, size);
+		newarray[i] = arr[minEL];
+		arr[minEL] = INT_MAX;
+	}
+
+
+	output(newarray, size);
+}
+
+
