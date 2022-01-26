b18f2dcc81fe7f85a334205ead9299673db09ea4
Infendum <69817375+Infendum@users.noreply.github.com>
2022-01-26 20:38:11 +0600

    Create 26.01.2022.cpp

---
+++ 26.01.2022.cpp
@@ -0,0 +1,31 @@
+задание номер 1
+#include <iostream>
+#include <Windows.h>
+
+
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	srand(4784);
+
+	const int size = 10;
+	int arr[3][3];
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			arr[i][j] = rand();
+			std::cout << arr[i][j] << "\t";
+		}
+		std::cout << "\n";
+		
+		
+
+	}
+
+	std::cout << arr[0][0] + arr[0][1] + arr[0][2];
+	std::cout << arr[1][0] + arr[1][1] + arr[1][2];
+	std::cout << arr[2][0] + arr[2][1] + arr[2][2];
+}
+
+задание номер 2
