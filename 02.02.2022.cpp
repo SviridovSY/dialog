863d8d9cac3ef1d001d81fd4b4d23c812cbfb301
Infendum <69817375+Infendum@users.noreply.github.com>
2022-02-02 20:42:58 +0600

    Update 02.02.2022.cpp

--- 02.02.2022.cpp
+++ 02.02.2022.cpp
@@ -38,3 +38,53 @@ int main() {
 	
 	std::cout << func() << std::endl;
 }
+
+
+
+задание номер 2
+	
+	
+#include <iostream>
+#include <Windows.h>
+
+
+int maximum(int(*a)[5]) {
+    int max = a[0][0];
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (a[i][j] > max) {
+                max = a[i][j];
+            }
+        }
+    }
+    return max;
+}
+
+int minimum(int(*a)[5]) {
+    int min = a[0][0];
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (a[i][j] < min) {
+                min = a[i][j];
+            }
+        }
+    }
+    return min;
+}
+
+int main() {
+	srand(4784);
+	int b = 0;
+	const int n = 5;
+	int a[n][n];
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			a[i][j] = rand() % (60 - 30 + 1) + 30;
+			
+			std::cout << a[i][j] << "\t";
+		}
+		std::cout << "\n" << "\n";
+	}
+    std::cout<< minimum <<std::endl;
+    std::cout << maximum << std::endl;
+}
