471680d17fe3ec14c6611c7588196bb3348b820b
Infendum <69817375+Infendum@users.noreply.github.com>
2022-02-20 23:57:37 +0600

    Create zadanie.cpp

---
+++ zadanie.cpp
@@ -0,0 +1,17 @@
+задание номер 1
+
+#include <iostream>
+
+
+
+int main() {
+	int a = 10;
+	int b = 0;
+	for (int i = 0; i <= 78; i++) {
+		b += a + i ;
+		
+		
+		std::cout << b << std::endl;
+	}
+	
+}
