9652104233fdc97161301804988da9c25cdc22d7
Infendum <69817375+Infendum@users.noreply.github.com>
2022-02-21 21:45:43 +0600

    Create 21.02.2022.cpp

---
+++ 21.02.2022.cpp
@@ -0,0 +1,178 @@
+#include <iostream>
+#include <Windows.h>
+
+
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	///*int a;                                    задание номер 1
+	//int b;
+	//int c;
+	//int y = 3;
+	//std::cin >> a;
+	//if (a > 3) {
+	//	b = a + 10;
+	//	std::cout << b << std::endl;
+	//}
+	//else if (a < y); {
+	//	c = a - 10;
+	//	std::cout << c << std::endl;
+	//}*/
+
+
+
+	/*int a                                       задание номер 2
+	int b;
+	int c;
+	std::cin >> a;
+	if (a < 7) {
+		std::cout << "Yes" << std::endl;
+	}
+	else if (a > 10) {
+		std::cout << "No" << std::endl;
+	}
+	else if (a == 9) {
+		std::cout << "Error" << std::endl;
+	}*/
+
+
+
+	/*int a;                             задание номер 3
+	std::cin >> a;
+	if (a == 1) {
+		std::cout << "Январь" << std::endl;
+	}
+	else if (a == 2) {
+		std::cout << "Февраль" << std::endl;
+	}
+	else if (a == 3) {
+		std::cout << "Март" << std::endl;
+	}
+	else if (a == 4) {
+		std::cout << "Апрель" << std::endl;
+	}
+	else if (a == 5) {
+		std::cout << "Май" << std::endl;
+	}
+	else if (a == 6) {
+		std::cout << "Июнь" << std::endl;
+	}
+	else if (a == 7) {
+		std::cout << "Июль" << std::endl;
+	}
+	else if (a == 8) {
+		std::cout << "Август" << std::endl;
+	}
+	else if (a == 9) {
+		std::cout << "Сентябрь" << std::endl;
+	}
+	else if (a == 10) {
+		std::cout << "Октябрь" << std::endl;
+	}
+	else if (a == 11) {
+		std::cout << "Ноябрь" << std::endl;
+	}
+	else if (a == 12) {
+		std::cout << "Декабрь" << std::endl;
+	}*/
+
+
+	/*int a;                                 задание номер 4
+	int b;
+	std::cin >> a >> b;
+	if (a > b) {
+		std::cout << a << std::endl;
+	}
+	else if (b > a) {
+		std::cout << b << std::endl;
+	}*/
+
+
+
+	/*int chislo{ 0 };                       задание номер 5
+	int chislo2{ 0 };
+	std::cin >> chislo;
+	std::cin >> chislo2;
+	if (chislo + 100 < chislo2 || chislo - 100 > chislo2  ) {
+		std::cout << "yes" << std::endl;
+	}
+	else  {
+		std::cout << "no" << std::endl;
+	}*/
+
+
+
+	/*int a;                              задание номер 6
+	int b;
+	int c;
+	cin >> a;
+	cin >> b;
+	if (a > b) {
+		cout << "yes" << endl;
+	}
+	else {
+		c = b;
+		b = a;
+		a = c;
+		cout << a << b << endl;
+		cout << "no" << endl;
+	}*/
+
+	/*int a;                                        задание номер 7
+	int b;
+	int c;
+	std::cin >> a;
+	if (-10 < a && a < 10) {
+		b = a + 5;
+		std::cout << b << std::endl;
+	}
+	else {
+		c = a - 10;
+		std::cout << c << std::endl;
+	}*/
+
+
+
+
+	/*int a;                         задание номер 8
+	int b;
+	int c;
+
+	std::cin >> a;
+	if (a > 100 || a < -100) {
+		b = a - a;
+		std::cout << b << std::endl;
+	}
+	else {
+		c = a + 1;
+		std::cout << c << std::endl;
+	}*/
+
+
+	/*for (int i = 1000; i < 10000; i++) {          задание номер 9
+		int a;
+		int b;
+		int c;
+		int d;
+		a = i % 10;
+		b = i % 100 / 10;
+		c = i % 1000 / 100;
+		d = i / 1000;
+
+
+
+		
+
+		if (a != b && a != c && a != d && c != b && d != c && b != d) {
+			std::cout << i << std::endl;
+		}
+
+
+
+
+
+
+
+	}*/
+}
