b47862c0fa7ae6933b69c76254ce81b3a02259ea
Infendum <69817375+Infendum@users.noreply.github.com>
2022-02-28 20:24:36 +0600

    Update 28.02.2022.cpp

--- 28.02.2022.cpp
+++ 28.02.2022.cpp
@@ -49,3 +49,50 @@ int main(void) {
 
 
 
+задание номер 3
+	
+	
+	
+#include <iostream>
+
+
+
+int func2(int x, int m2) {
+	m2 = x % 1000 / 100;
+	m2 = m2 * 100;
+	
+	return m2;
+	
+}
+
+
+
+
+int func1(int x, int m1) {
+	
+	m1 = x % 100 / 10;
+	m1 = m1 * 10;
+	return m1;
+	
+}
+
+
+int func(int x,  int m) {
+
+	m = x % 10;
+	
+	return m;
+}
+
+
+int main(void) {
+	int a{ 0 }, b{ 0 }, d{ 0 }, j{ 0 }, k{ 0 }, l, v;
+	std::cin >> a;
+
+	b = func(a, k);
+	l = func1(a, j);
+	v = func2(a, d);
+	std::cout << v <<'\n' << l << '\n' << b << '\n' << std::endl;
+
+	return 0;
+}
