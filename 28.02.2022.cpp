90ff5d1e7691450cd2e0b34168773a99eec14cea
Infendum <69817375+Infendum@users.noreply.github.com>
2022-02-28 20:54:57 +0600

    Update 28.02.2022.cpp

--- 28.02.2022.cpp
+++ 28.02.2022.cpp
@@ -96,3 +96,25 @@ int main(void) {
 
 	return 0;
 }
+
+
+
+задание номер 4
+	
+	
+#include <iostream>
+#include<cmath>
+
+int func1(int m2 = 5, int m1 = 6, int m0 = 7) {
+	int answer = 0;
+	answer += m2 * 100;
+	answer += m1 * 10;
+	answer += m0;
+	return answer;
+}
+
+int main() {
+	int a{ 4 }, b{ 3 }, c{ 8 }, ans{ 0 };
+	ans = func1();
+	std::cout << ans << std::endl;
+}
