f5e260a1cc58f88588dc3e8991ff562337dfa933
Infendum <69817375+Infendum@users.noreply.github.com>
2022-03-01 21:48:55 +0600

    Create zzadanie.cpp

---
+++ zzadanie.cpp
@@ -0,0 +1,21 @@
+задание номер 1
+  
+  
+int a;
+	std::cout << "Vvedite chilslo: ";
+	std::cin >> a;
+	int n = 1;
+	int sum = 0;
+	for (int i = 0; i < a; i++) {
+		sum += n;
+		n += 1;
+	}
+
+	std::cout << sum << std::endl;
+
+
+
+
+задание номер 2
+  
+  
