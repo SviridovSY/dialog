87af486cb6e0fbe502d95078e24bd26623c94ad8
Infendum <69817375+Infendum@users.noreply.github.com>
2022-03-07 20:43:01 +0600

    Update 07.03.2022.cpp

--- 07.03.2022.cpp
+++ 07.03.2022.cpp
@@ -3,57 +3,35 @@
 
 
 
+
 int krestik(int player) {
-	for (int i = 1; i <= 9; i++) {
-		std::cin >> player;
-		if (player == 1) {
-			std::cout << "*   *" << std::endl;
-			std::cout << "  *  " << std::endl;
-			std::cout << "*   *" << std::endl;
-		}
-		else if (player == 2) {
-			std::cout << "*   *" << std::endl;
-			std::cout << "  *  " << std::endl;
-			std::cout << "*   *" << std::endl;
-		}
-		else if (player == 3) {
-			std::cout << "*   *" << std::endl;
-			std::cout << "  *  " << std::endl;
-			std::cout << "*   *" << std::endl;
-		}
-		else if (player == 4) {
-			std::cout << "*   *" << std::endl;
-			std::cout << "  *  " << std::endl;
-			std::cout << "*   *" << std::endl;
-		}
-		else if (player == 5) {
-			std::cout << "*   *" << std::endl;
-			std::cout << "  *  " << std::endl;
-			std::cout << "*   *" << std::endl;
-		}
-		else if (player == 6) {
-			std::cout << "*   *" << std::endl;
-			std::cout << "  *  " << std::endl;
-			std::cout << "*   *" << std::endl;
-		}
-		else if (player == 7) {
-			std::cout << "*   *" << std::endl;
-			std::cout << "  *  " << std::endl;
-			std::cout << "*   *" << std::endl;
-		}
-		else if (player == 89) {
-			std::cout << "*   *" << std::endl;
-			std::cout << "  *  " << std::endl;
-			std::cout << "*   *" << std::endl;
-		}
-		else if (player == 9) {
-			std::cout << "*   *" << std::endl;
-			std::cout << "  *  " << std::endl;
-			std::cout << "*   *" << std::endl;
-		}
-	}
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	
+
+			std::cout << ' ' << '*' <<' ' << '*' << ' ' << std::endl;
+			std::cout << ' ' << ' ' << "*" << ' ' << ' ' << std::endl;
+			std::cout << ' ' << '*' << ' ' << '*' << ' ' << std::endl;
+		
+	
 	return 0;
 }
+
+int nolik(int player) {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+
+
+	std::cout << ' ' << '*' << '*' << '*' << ' ' << std::endl;
+	std::cout << ' ' << '*' << ' ' << '*' << ' ' << std::endl;
+	std::cout << ' ' << '*' << '*' << '*' << ' ' << std::endl;
+
+
+	return 0;
+}
+
 
 
 int main() 
