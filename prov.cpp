f586866cb32005d659481cd400943740c79e82d3
Infendum <69817375+Infendum@users.noreply.github.com>
2022-03-09 20:37:26 +0600

    Create prov.cpp

---
+++ prov.cpp
@@ -0,0 +1,239 @@
+#include <iostream>
+#include <Windows.h>
+
+
+
+//int krestik() {
+//	SetConsoleCP(1251);
+//	SetConsoleOutputCP(1251);
+//
+//
+//
+//	std::cout << ' ' << '*' << ' ' << '*' << ' ' << std::endl;
+//	std::cout << ' ' << ' ' << "*" << ' ' << ' ' << std::endl;
+//	std::cout << ' ' << '*' << ' ' << '*' << ' ' << std::endl;
+//	return 0;
+//
+//}
+//
+//int nolik() {
+//	SetConsoleCP(1251);
+//	SetConsoleOutputCP(1251);
+//
+//
+//
+//	std::cout << ' ' << '*' << '*' << '*' << ' ' << std::endl;
+//	std::cout << ' ' << '*' << ' ' << '*' << ' ' << std::endl;
+//	std::cout << ' ' << '*' << '*' << '*' << ' ' << std::endl;
+//	return 0;
+//
+//}
+
+
+
+//int vstav(int x, int y) {
+//	COORD p = { x, y };
+//	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), p);
+//
+//	return 0;
+//}
+
+
+//void gotoxy(int x, int y) {
+//	COORD p = { x, y };
+//	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), p);
+//}
+
+
+void pole()
+{
+
+}
+
+
+void gridold() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	std::cout << "|" << ' ' << ' ' << ' ' << ' ' << ' ' << '|'
+		<< "|" << ' ' << ' ' << ' ' << ' ' << ' ' << '|'
+		<< "|" << ' ' << ' ' << ' ' << ' ' << ' ' << '|' << std::endl
+
+		<< "|" << ' ' << ' ' << ' ' << ' ' << ' ' << '|'
+		<< "|" << ' ' << ' ' << ' ' << ' ' << ' ' << '|'
+		<< "|" << ' ' << ' ' << ' ' << ' ' << ' ' << '|' << std::endl
+
+		<< "|" << "_" << "_" << "_" << "_" << "_" << '|'
+		<< "|" << "_" << "_" << "_" << "_" << "_" << '|'
+		<< "|" << "_" << "_" << "_" << "_" << "_" << '|' << std::endl;
+
+	std::cout << "|" << ' ' << ' ' << ' ' << ' ' << ' ' << '|'
+		<< "|" << ' ' << ' ' << ' ' << ' ' << ' ' << '|'
+		<< "|" << ' ' << ' ' << ' ' << ' ' << ' ' << '|' << std::endl
+
+		<< "|" << ' ' << ' ' << ' ' << ' ' << ' ' << '|'
+		<< "|" << ' ' << ' ' << ' ' << ' ' << ' ' << '|'
+		<< "|" << ' ' << ' ' << ' ' << ' ' << ' ' << '|' << std::endl
+
+		<< "|" << "_" << "_" << "_" << "_" << "_" << '|'
+		<< "|" << "_" << "_" << "_" << "_" << "_" << '|'
+		<< "|" << "_" << "_" << "_" << "_" << "_" << '|' << std::endl;
+
+	std::cout << "|" << ' ' << ' ' << ' ' << ' ' << ' ' << '|'
+		<< "|" << ' ' << ' ' << ' ' << ' ' << ' ' << '|'
+		<< "|" << ' ' << ' ' << ' ' << ' ' << ' ' << '|' << std::endl
+
+		<< "|" << ' ' << ' ' << ' ' << ' ' << ' ' << '|'
+		<< "|" << ' ' << ' ' << ' ' << ' ' << ' ' << '|'
+		<< "|" << ' ' << ' ' << ' ' << ' ' << ' ' << '|' << std::endl
+
+		<< "|" << "_" << "_" << "_" << "_" << "_" << '|'
+		<< "|" << "_" << "_" << "_" << "_" << "_" << '|'
+		<< "|" << "_" << "_" << "_" << "_" << "_" << '|' << std::endl;
+
+	std::cout << "1  2  3" << std::endl;
+	std::cout << "4  5  6" << std::endl;
+	std::cout << "7  8  9" << std::endl;
+	std::cout << "Для вывода крестика или нолика ваот кординат клеток 1(0,0) 2()" << std::endl;
+}
+void cross(int coord)
+{
+	int x{ 0 }, y{ 0 };
+	if (coord == 1) {
+		x = 1;
+		y = 0;
+	}
+	else if (coord == 2) {
+		x = 8;
+		y = 0;
+	}
+	else if (coord == 3) {
+		x = 15;
+		y = 0;
+	}
+	else if (coord == 4) {
+		x = 1;
+		y = 3;
+	}
+	else if (coord == 5) {
+		x = 8;
+		y = 3;
+	}
+	else if (coord == 6) {
+		x = 15;
+		y = 3;
+	}
+	else if (coord == 7) {
+		x = 1;
+		y = 6;
+	}
+	else if (coord == 8) {
+		x = 8;
+		y = 6;
+	}
+	else if (coord == 9) {
+		x = 15;
+		y = 6;
+	}
+	gotoxy(x, y);
+	std::cout << ' ' << '*' << ' ' << '*' << ' ' << std::endl;
+	gotoxy(x, y + 1);
+	std::cout << ' ' << ' ' << "*" << ' ' << ' ' << std::endl;
+	gotoxy(x, y + 2);
+	std::cout << ' ' << '*' << ' ' << '*' << ' ' << std::endl;
+}
+
+
+
+
+void gotoxy(int x, int y) {
+	COORD p = { x, y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), p);
+
+}
+
+void gotoxy2(int x, int y) {
+	COORD p = { x, y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), p);
+
+	std::cout << ' ' << '*' << '*' << '*' << ' ' << std::endl;
+	std::cout << ' ' << '*' << ' ' << '*' << ' ' << std::endl;
+	std::cout << ' ' << '*' << '*' << '*' << ' ' << std::endl;
+
+
+
+}
+
+
+
+int main(void)
+{
+	gridold();
+	int a{ 0 }, u{ 0 };
+	int b;
+	int g;
+
+
+
+	int j;
+	std::cin >> j;
+	for (int i = 0; i < j; i++) {
+		std::cin >> a;
+		std::cin >> u;
+		gotoxy(a, u);
+		std::cout << std::endl;
+		std::cout << std::endl;
+		std::cout << std::endl;
+		std::cout << std::endl;
+		std::cout << std::endl;
+		std::cout << std::endl;
+		std::cout << std::endl;
+		std::cout << std::endl;
+		std::cout << std::endl;
+		std::cout << std::endl;
+		std::cout << std::endl;
+		std::cout << std::endl;
+		std::cout << std::endl;
+		std::cout << std::endl;
+		std::cout << std::endl;
+		std::cout << std::endl;
+		std::cout << std::endl;
+		std::cout << std::endl;
+		std::cout << std::endl;
+		std::cout << std::endl;
+		std::cout << std::endl;
+
+		std::cin >> b;
+		std::cin >> g;
+
+		gotoxy2(b, g);
+		std::cout << std::endl;
+		std::cout << std::endl;
+		std::cout << std::endl;
+		std::cout << std::endl;
+		std::cout << std::endl;
+		std::cout << std::endl;
+		std::cout << std::endl;
+		std::cout << std::endl;
+		std::cout << std::endl;
+		std::cout << std::endl;
+		std::cout << std::endl;
+		std::cout << std::endl;
+		std::cout << std::endl;
+		std::cout << std::endl;
+		std::cout << std::endl;
+		std::cout << std::endl;
+		std::cout << std::endl;
+		std::cout << std::endl;
+		std::cout << std::endl;
+		std::cout << std::endl;
+		std::cout << std::endl;
+		std::cout << std::endl;
+		std::cout << std::endl;
+		std::cout << std::endl;
+		std::cout << std::endl;
+		/*j = vstav(a, u);*/
+	}
+
+
+	return 0;
+}
