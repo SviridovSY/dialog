94fe9b05a8f791a71193e8210f6dab1b26bf8a2a
Infendum <69817375+Infendum@users.noreply.github.com>
2022-03-14 21:26:10 +0600

    Create 14.03(2).2022.cpp

---
+++ 14.03(2).2022.cpp
@@ -0,0 +1,11 @@
+int ii(int n[3][3] = 0) {
+	const int cross = 1;
+	const int zero = 2;
+	n[1][1] = cross;
+	if (zero == n[0][1] /*|| zero == n[1][0] || zero == n[1][2] || zero == n[2][1]*/) {
+		n[0][2] = cross;
+		if (cross == n[0][2] || cross == n[2][2]);
+		
+	}
+	if()
+}
